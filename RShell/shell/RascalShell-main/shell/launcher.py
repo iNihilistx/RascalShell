27da89fda9ce9a500c2c519c76ecc66940aff633
iNihilist <64751372+iNihilistx@users.noreply.github.com>
2024-06-17 03:26:54 +0100

    Delete RShell directory

--- RShell/shell/RascalShell-main/shell/launcher.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def main():
-    # run the updater first
-    subprocess.run(["python", "update_checker.py"])
-    subprocess.run(["python", "portal.py"])
-
-if __name__ == "__main__":
-    main()
